2ada55980269fae9442e5673d054ab70d982a2d7
Sneha G A <snehaambiga2006@gmail.com>
2025-12-13 22:58:07 +0530

    Add C solution for Palindrome Number (Day 01)

---
+++ Leetcode/09_Palindrome_Number.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+#include <limits.h>
+
+bool isPalindrome(int x) {
+   
+    if (x < 0 || (x > 0 && x % 10 == 0)) {
+        return false;
+    }
+
+    int reversed_half = 0;
+    
+    while (x > reversed_half) {
+        reversed_half = reversed_half * 10 + x % 10;
+        x /= 10;
+    }
+
+    return x == reversed_half || x == reversed_half / 10;
+}
