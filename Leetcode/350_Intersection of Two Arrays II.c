eaf67fb6f9e749727f51eea15506ac2ae345ccab
Sneha G A <snehaambiga2006@gmail.com>
2026-01-01 23:54:45 +0530

    LeetCode 350 in C - DSA December(Day -20)

---
+++ Leetcode/350_Intersection of Two Arrays II.c
@@ -0,0 +1,25 @@
+int cmp(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+    qsort(nums1, nums1Size, sizeof(int), cmp);
+    qsort(nums2, nums2Size, sizeof(int), cmp);
+
+    int *result = (int *)malloc(sizeof(int) * (nums1Size < nums2Size ? nums1Size : nums2Size));
+    int i = 0, j = 0, k = 0;
+
+    while (i < nums1Size && j < nums2Size) {
+        if (nums1[i] == nums2[j]) {
+            result[k++] = nums1[i];
+            i++;
+            j++;
+        } else if (nums1[i] < nums2[j]) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+
+    *returnSize = k;
+    return result;
+}
